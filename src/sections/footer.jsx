22652bc4d0e917224e2be6a7b63f3c75f65f17c5
Beki Junior <bbekijunior1@gmail.com>
2025-06-05 11:02:14 +0300

    adding footer

---
+++ src/sections/footer.jsx
@@ -0,0 +1,25 @@
+import { Box, Typography } from "@mui/material";
+const Footer = () => {
+    return (
+        <Box
+            component="footer"
+            sx={{
+                backgroundColor: "#f5f5f5",
+                padding: "1.5rem",
+                textAlign: "center",
+                mt: "auto",
+            }}
+        >
+            <Typography variant="body2" color="textSecondary">
+                <Box display={'inline-flex'} justifyContent={'space-between'}>
+                    <Box>
+                        Developed by Bereket Tsegaye .
+                    </Box>
+                    <Box>  © {new Date().getFullYear()} All rights reserved.</Box>
+                </Box>
+            </Typography>
+        </Box>
+    );
+}
+
+export default Footer;
